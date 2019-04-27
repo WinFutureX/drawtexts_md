25a768ec819eb58ccac6998c513d59c989e47abc
Kelsey Boey <kelseyboey@hotmail.com>
2019-04-27 20:33:53 +0800

    update to private rev

--- drawtexts.asm
+++ drawtexts.asm
@@ -1,9 +1,10 @@
 ; text drawing system for sega mega drive
 ; prints out null-terminated strings to the screen
+; copyfuck 2018-42069 kelsey boey
 
-; ============================
-; = ALL THAT NECESSARY STUFF =
-; ============================
+; ****************************
+; * ALL THAT NECESSARY STUFF *
+; ****************************
 
 ; reset system stack pointer
 initialssp:	equ	$8FFFFF00
@@ -37,11 +38,11 @@ vectors:
 		dc.l	cpufault	;  7: trapv instruction
 		dc.l	cpufault	;  8: privilege violation
 		dc.l	cpufault	;  9: trace
-		dc.l	cpufault	; 10: line A trap
-		dc.l	cpufault	; 11: line F trap
+		dc.l	cpufault	; 10: line A emulator
+		dc.l	cpufault	; 11: line F emulator
 		dc.l	cpufault	; 12: unassigned, reserved
 		dc.l	cpufault	; 13: unassigned, reserved
-		dc.l	cpufault	; 14: format error (68010 and up)
+		dc.l	cpufault	; 14: format error (68010)
 		dc.l	cpufault	; 15: uninitialized interrupt vector
 		dc.l	cpufault	; 16: unassigned, reserved
 		dc.l	cpufault	; 17: unassigned, reserved
@@ -95,8 +96,8 @@ vectors:
 ; game header info
 header:
 		dc.b	"SEGA MEGA DRIVE "	; console name
-		dc.b	"                "	; copyright name/date
-		dc.b	"domestic name                                   "
+		dc.b	"(C) KELSEY BOEY "	; copyright name/date
+		dc.b	"some 68k code or something, i don't fucking know"
 		dc.b	"TEXT DRAWING MECHANISM                          "
 		dc.b	"GM 00000000-00"	; product no.
 		dc.w	$0			; checksum
@@ -108,7 +109,7 @@ header:
 		dc.l	$20202020		; sram support (disabled)
 		dc.l	$20202020		; sram end
 		dc.l	$20202020		; modem
-		dc.b	"notes will go here                                  "
+		dc.b	"according to all known laws of aviation, there is no"
 		dc.b	"JUE             "	; region
 
 ; 68k exception handler (freezes the cpu)
@@ -119,19 +120,14 @@ cpufault:
 
 ; 68k code entry point
 startup_68k:
-		move.w	#$2700, sr		; disable ints
-		tst.l	$A10008			; test port A & B
-		bne.s	initz80
-		tst.l	$A1000C			; test port C
-		bne.s	initz80			; was it a soft reset?
 		move.b	$A10001, d0		; get HW ver
 		andi.b	#$0F, d0		; compare to rev 0
 		beq.s	initz80			; for non-TMSS systems only
 		move.l	#"SEGA", $A14000	; make the TMSS happy
 
-;==================================
-;= DEVICE INITIALIZATION PAYLOADS =
-;==================================
+; **********************************
+; * DEVICE INITIALIZATION PAYLOADS *
+; **********************************
 
 ; startup the z80
 initz80:
@@ -153,29 +149,29 @@ z80loop:
 ; z80 startup instructions		
 z80code:
 		dc.b	$AF		; xor	a
-		dc.b	$01, $D9, $1F	; ld	bc,1fd9h
-		dc.b	$11, $27, $00	; ld	de,0027h
-		dc.b	$21, $26, $00	; ld	hl,0026h
-		dc.b	$F9		; ld	sp,hl
-		dc.b	$77		; ld	(hl),a
+		dc.b	$01, $D9, $1F	; ld	bc, 1fd9h
+		dc.b	$11, $27, $00	; ld	de, 0027h
+		dc.b	$21, $26, $00	; ld	hl, 0026h
+		dc.b	$F9		; ld	sp, hl
+		dc.b	$77		; ld	(hl), a
 		dc.b	$ED, $B0	; ldir
 		dc.b	$DD, $E1	; pop	ix
 		dc.b	$FD, $E1	; pop	iy
-		dc.b	$ED, $47	; ld	i,a
-		dc.b	$ED, $4F	; ld	r,a
+		dc.b	$ED, $47	; ld	i, a
+		dc.b	$ED, $4F	; ld	r, a
 		dc.b	$D1		; pop	de
 		dc.b	$E1		; pop	hl
 		dc.b	$F1		; pop	af
-		dc.b	$08		; ex	af,af'
+		dc.b	$08		; ex	af, af'
 		dc.b	$D9		; exx
 		dc.b	$C1		; pop	bc
 		dc.b	$D1		; pop	de
 		dc.b	$E1		; pop	hl
 		dc.b	$F1		; pop	af
-		dc.b	$F9		; ld	sp,hl
+		dc.b	$F9		; ld	sp, hl
 		dc.b	$F3		; di
 		dc.b	$ED, $56	; im1
-		dc.b	$36, $E9	; ld	(hl),e9h
+		dc.b	$36, $E9	; ld	(hl), e9h
 		dc.b	$E9		; jp	(hl)
 		
 z80end:
@@ -185,13 +181,14 @@ z80end:
 ; setup vdp (320x224 resolution, 40 col x 28 lines)
 initvdp:
 		lea	vdpctrl, a0		; target vdp control
+		lea	vdpdata, a1		; target vdp data
 		move.l	#$80048154, (a0)	; reg $80/81
 		move.l	#$82308340, (a0)	; reg $82/83
 		move.l	#$8407856A, (a0)	; reg $84/85
 		move.l	#$86008700, (a0)	; reg $86/87
 		move.l	#$8A008B08, (a0)	; reg $8A/8B
 		move.l	#$8C898D34, (a0)	; reg $8C/8D
-		move.l	#$8E008F00, (a0)	; reg $8E/8F
+		move.l	#$8E008F02, (a0)	; reg $8E/8F
 		move.l	#$90019200, (a0)	; reg $90/92
 		move.l	#$93009400, (a0)	; reg $93/94
 		move.l	#$95009700, (a0)	; reg $95/97
@@ -230,23 +227,23 @@ initjoy:
 		move.b	#0, expctrl
 
 laststeps:
-		move.w	#50-1, d0	; stop reset bug?
+		move.w	(a0), d0	; stop reset bug?
 		move.l	$0, a6
 		move.l	a6, usp		; zero out usp (will this work?)
 		movem.l (a6), d0-a6	; clear all registers (except sp)
 		jmp	main		; here we go
 
-;===============================
-;= VDP AND EXTERNAL INTERRUPTS =
-;===============================
+; *******************************
+; * VDP AND EXTERNAL INTERRUPTS *
+; *******************************
 
 ; we're not using any interrupts yet
 useless:
 		rte
 
-;=====================
-;= MAIN PROGRAM CODE =
-;=====================
+; *********************
+; * MAIN PROGRAM CODE *
+; *********************
 
 main:
 		; reinitialize vdp
@@ -332,9 +329,9 @@ done:
 		bra.s	*	; hang
 
 ; -SUBROUTINE-
-; ===================================================
+; ***************************************************
 ; drawstr - draws and prints a null-terminated string
-; ===================================================
+; ***************************************************
 ; arguments:
 ; d0.w: x-coord
 ; d1.w: y-coord
@@ -342,7 +339,7 @@ done:
 ; d3.w: no. of tiles to load
 ; a2.l: pointer to string
 ; a3.l: tile data address
-; ===================================================
+; ***************************************************
 ; TRASHES d0, d1, d2, d3, d4, d7, a2, a3 & a4
 drawstr:
 		; determine where to write
@@ -351,7 +348,7 @@ drawstr:
 		add.w	d0, d0
 		andi.l	#$FFFF, d0
 		swap	d0
-		ori.l	#$60000003, d0
+		ori.l	#$40000003, d0
 		move.l	d0, vdpctrl
 		
 		; get ascii lookup table addr
@@ -415,11 +412,12 @@ loadtilesloop:
 		dbf	d7, loadtilesinnerloop
 		move.l	d4, vdpdata		; send line to vdp
 		dbf	d3, loadtilesloop	; next line
+		
 		rts				; end of subroutine
 
-; =================
-; = ROM DATA AREA =
-; =================
+; *****************
+; * ROM DATA AREA *
+; *****************
 
 ; font section
 font:		incbin	"pixelfont.bin"
@@ -428,18 +426,18 @@ fontsize:	equ	(*-font)/$08
 
 ; ascii conversion lookup table
 asciitable:
-		dc.w	$0000,$0040,$0041,$0042,$0043,$0044,$0045,$0046	; $20 - $27
-		dc.w	$0047,$0048,$0049,$004A,$004B,$004C,$004D,$004E	; $28 - $2F
-		dc.w	$0001,$0002,$0003,$0004,$0005,$0006,$0007,$0008	; $30 - $37
-		dc.w	$0009,$000A,$004F,$0050,$0051,$0052,$0053,$0054	; $38 - $3F
-		dc.w	$0055,$000B,$000C,$000D,$000E,$000F,$0010,$0011	; $40 - $47
-		dc.w	$0012,$0013,$0014,$0015,$0016,$0017,$0018,$0019	; $48 - $4F
-		dc.w	$001A,$001B,$001C,$001D,$001E,$001F,$0020,$0021	; $50 - $57
-		dc.w	$0022,$0023,$0024,$0056,$0057,$0058,$0059,$005A	; $58 - $5F
-		dc.w	$005B,$0025,$0026,$0027,$0028,$0029,$002A,$002B	; $60 - $67
-		dc.w	$002C,$002D,$002E,$002F,$0030,$0031,$0032,$0033	; $68 - $6F
-		dc.w	$0034,$0035,$0036,$0037,$0038,$0039,$003A,$003B	; $70 - $77
-		dc.w	$003C,$003D,$003E,$005C,$005D,$005E,$005F,$0000	; $78 - $7F
+		dc.w	$0000, $0040, $0041, $0042, $0043, $0044, $0045, $0046	; $20 - $27
+		dc.w	$0047, $0048, $0049, $004A, $004B, $004C, $004D, $004E	; $28 - $2F
+		dc.w	$0001, $0002, $0003, $0004, $0005, $0006, $0007, $0008	; $30 - $37
+		dc.w	$0009, $000A, $004F, $0050, $0051, $0052, $0053, $0054	; $38 - $3F
+		dc.w	$0055, $000B, $000C, $000D, $000E, $000F, $0010, $0011	; $40 - $47
+		dc.w	$0012, $0013, $0014, $0015, $0016, $0017, $0018, $0019	; $48 - $4F
+		dc.w	$001A, $001B, $001C, $001D, $001E, $001F, $0020, $0021	; $50 - $57
+		dc.w	$0022, $0023, $0024, $0056, $0057, $0058, $0059, $005A	; $58 - $5F
+		dc.w	$005B, $0025, $0026, $0027, $0028, $0029, $002A, $002B	; $60 - $67
+		dc.w	$002C, $002D, $002E, $002F, $0030, $0031, $0032, $0033	; $68 - $6F
+		dc.w	$0034, $0035, $0036, $0037, $0038, $0039, $003A, $003B	; $70 - $77
+		dc.w	$003C, $003D, $003E, $005C, $005D, $005E, $005F, $0000	; $78 - $7F
 		; translated:
 		; [space], !, ", #, $, %, &, '
 		; (, ), *, +, [comma], -, ., /
